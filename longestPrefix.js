755d832009a4ac1e4f1290db980844576118a8f3
Suparna Dhar <suparnad806@gmail.com>
2025-02-23 11:48:23 +0600

    longest prefix

---
+++ longestPrefix.js
@@ -0,0 +1,19 @@
+function longestCommonPrefix(strs) {
+  if (!strs.length) return "";
+
+  let prefix = strs[0]; 
+  for (let i = 1; i < strs.length; i++) {
+    while (strs[i].indexOf(prefix) !== 0) {
+      prefix = prefix.slice(0, -1); 
+      if (!prefix) return "";
+    }
+  }
+
+  return prefix;
+}
+
+// Example Test Cases
+console.log(longestCommonPrefix(["flower", "flow", "flight"])); 
+console.log(longestCommonPrefix(["dog", "racecar", "car"])); 
+console.log(longestCommonPrefix(["apple", "app", "april"])); 
+console.log(longestCommonPrefix(["interview", "internet", "internal"])); 
